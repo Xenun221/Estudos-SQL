97112b54de1f9743d4fbce31a0ebe701c65875c7
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-05-20 22:59:15 -0300

    Update 12-Usando o Delete.sql

--- 12-Usando o Delete.sql
+++ 12-Usando o Delete.sql
@@ -3,4 +3,6 @@ DELETE FROM tabelafornecedores WHERE pais_de_origem ='Turquia'
 /*
 Sempre Usar um From no delete
 
+
+O comando DROP TABLE exclui por completo uma tabela do banco de dados. O comando DELETE exclui apenas os registros armazenados em uma tabela do banco de dados
 */
