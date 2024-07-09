810677aad61818a49da3ca4a586d9adfc8f01954
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-07-09 19:40:40 -0300

    Aula38 Utilizando view
    Aula38 de como fazer o uso de uma view

---
+++ 37-Utilizando Views.sql
@@ -0,0 +1,11 @@
+/*Posso aplicar somente em uma coluna*/
+SELECT nome
+FROM ViewValorTotalPedido
+
+
+/*Aplicar filtro*/
+SELECT *
+FROM ViewValorTotalPedido
+WHERE Soma Dos pedidos = 10Soma dos pedidos
+
+/*Conseguimos aplicar os mesmos filtros quando trabalhamos com uma tabela fisica*/
